afe663aeda0506949d826c4dda8a0fb1ea89fb55
Swiftzerr <elliotjarnit@gmail.com>
2022-09-27 22:26:15 -0700

    added home and mailing list join pages

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button class="waitlist" @click="waitlistRedirect">Join Waitlist</button>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    methods: {
+        waitlistRedirect() {
+            window.location.href = '/waitlist'
+        }
+    }
+}
+</script>
---
+++ pages/join.vue
@@ -0,0 +1,38 @@
+<template>
+    <form>
+        <inputbox value="Email" v-model="email" required email></inputbox>
+        <inputbox value="First Name" v-model="firstName" required></inputbox>
+        <inputbox value="Last Name" v-model="lastName" required></inputbox>
+        <inputbox value="Phone Number" v-model="phone" required></inputbox>
+        <inputbox value="Street" v-model="street" required></inputbox>
+        <inputbox value="Apartment" v-model="apartment"></inputbox>
+        <inputbox value="City" v-model="city" required></inputbox>
+        <inputbox value="Zip Code" v-model="zip" required></inputbox>
+        <button type="submit" v-on:click.prevent="submit">Submit</button>
+    </form>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            email: "",
+            firstName: "",
+            lastName: "",
+            phone: "",
+            street: "",
+            apartment: "",
+            city: "",
+            zip: ""
+        }
+    },
+    methods: {
+        test() {
+            console.log(this.email);
+        },
+        async submit() {
+            console.log(this.email)
+        }
+    }
+}
+</script>
