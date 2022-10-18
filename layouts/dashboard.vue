5b7766ef28dc6b4d6da37ee5fd6f7e3fdd851653
Swiftzerr <elliotjarnit@gmail.com>
2022-10-17 22:15:08 -0700

    dashboard work

---
+++ layouts/dashboard.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="sidebar">
+        <div class="sidebar-content">
+            <div class="sidebar-content-item">
+                <div class="sidebar-content-item-title">
+                    Users
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="rest">
+        <slot />
+    </div>
+</template>
+
+<style>
+.sidebar {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 10%;
+    background-color: #f8f9fa;
+    z-index: 100;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    transition: all 0.5s ease;
+}
+
+
+
+
+
+.rest {
+    position: absolute;
+    top: 0;
+    left: 10%;
+    height: 100%;
+    width: 90%;
+    transition: all 0.5s ease;
+}
+</style>
